2081b50829bce4d2254d18fa31ef824845c5b71f
mytra <163012214+micha100points@users.noreply.github.com>
2025-04-10 10:08:44 +0700

    Add files via upload

---
+++ 8_Puzzle/8_Puzzle/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace _8_Puzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            bt1 = new Button();
+            bt2 = new Button();
+            bt3 = new Button();
+            bt4 = new Button();
+            bt5 = new Button();
+            bt6 = new Button();
+            bt7 = new Button();
+            bt8 = new Button();
+            textBoxTime = new TextBox();
+            textBoxStep = new TextBox();
+            btMix = new Button();
+            btSolve = new Button();
+            btExit = new Button();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50.35971F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 49.64029F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 134F));
+            tableLayoutPanel1.Controls.Add(bt1, 0, 0);
+            tableLayoutPanel1.Controls.Add(bt2, 1, 0);
+            tableLayoutPanel1.Controls.Add(bt3, 2, 0);
+            tableLayoutPanel1.Controls.Add(bt4, 0, 1);
+            tableLayoutPanel1.Controls.Add(bt5, 1, 1);
+            tableLayoutPanel1.Controls.Add(bt6, 2, 1);
+            tableLayoutPanel1.Controls.Add(bt7, 0, 2);
+            tableLayoutPanel1.Controls.Add(bt8, 1, 2);
+            tableLayoutPanel1.Location = new Point(171, 176);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50.23256F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 49.76744F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 96F));
+            tableLayoutPanel1.Size = new Size(413, 307);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // bt1
+            // 
+            bt1.Font = new Font("Segoe UI Black", 27.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            bt1.Location = new Point(3, 3);
+            bt1.Name = "bt1";
+            bt1.Size = new Size(134, 99);
+            bt1.TabIndex = 0;
+            bt1.Text = "1";
+            bt1.UseVisualStyleBackColor = true;
+            // 
+            // bt2
+            // 
+            bt2.Font = new Font("Segoe UI Black", 27.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            bt2.Location = new Point(143, 3);
+            bt2.Name = "bt2";
+            bt2.Size = new Size(132, 99);
+            bt2.TabIndex = 1;
+            bt2.Text = "2";
+            bt2.UseVisualStyleBackColor = true;
+            // 
+            // bt3
+            // 
+            bt3.Font = new Font("Segoe UI Black", 27.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            bt3.Location = new Point(281, 3);
+            bt3.Name = "bt3";
+            bt3.Size = new Size(129, 99);
+            bt3.TabIndex = 2;
+            bt3.Text = "3";
+            bt3.UseVisualStyleBackColor = true;
+            // 
+            // bt4
+            // 
+            bt4.Font = new Font("Segoe UI Black", 27.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            bt4.Location = new Point(3, 108);
+            bt4.Name = "bt4";
+            bt4.Size = new Size(134, 99);
+            bt4.TabIndex = 3;
+            bt4.Text = "4";
+            bt4.UseVisualStyleBackColor = true;
+            // 
+            // bt5
+            // 
+            bt5.Font = new Font("Segoe UI Black", 27.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            bt5.Location = new Point(143, 108);
+            bt5.Name = "bt5";
+            bt5.Size = new Size(132, 99);
+            bt5.TabIndex = 4;
+            bt5.Text = "5";
+            bt5.UseVisualStyleBackColor = true;
+            // 
+            // bt6
+            // 
+            bt6.Font = new Font("Segoe UI Black", 27.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            bt6.Location = new Point(281, 108);
+            bt6.Name = "bt6";
+            bt6.Size = new Size(129, 99);
+            bt6.TabIndex = 5;
+            bt6.Text = "6";
+            bt6.UseVisualStyleBackColor = true;
+            // 
+            // bt7
+            // 
+            bt7.Font = new Font("Segoe UI Black", 27.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            bt7.Location = new Point(3, 213);
+            bt7.Name = "bt7";
+            bt7.Size = new Size(134, 91);
+            bt7.TabIndex = 6;
+            bt7.Text = "7";
+            bt7.UseVisualStyleBackColor = true;
+            // 
+            // bt8
+            // 
+            bt8.Font = new Font("Segoe UI Black", 27.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            bt8.Location = new Point(143, 213);
+            bt8.Name = "bt8";
+            bt8.Size = new Size(132, 91);
+            bt8.TabIndex = 7;
+            bt8.Text = "8";
+            bt8.UseVisualStyleBackColor = true;
+            // 
+            // textBoxTime
+            // 
+            textBoxTime.BackColor = SystemColors.GradientActiveCaption;
+            textBoxTime.Font = new Font("Segoe UI Semibold", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            textBoxTime.Location = new Point(677, 206);
+            textBoxTime.Multiline = true;
+            textBoxTime.Name = "textBoxTime";
+            textBoxTime.Size = new Size(123, 41);
+            textBoxTime.TabIndex = 1;
+            // 
+            // textBoxStep
+            // 
+            textBoxStep.BackColor = SystemColors.GradientActiveCaption;
+            textBoxStep.Font = new Font("Segoe UI Semibold", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            textBoxStep.Location = new Point(894, 206);
+            textBoxStep.Multiline = true;
+            textBoxStep.Name = "textBoxStep";
+            textBoxStep.Size = new Size(125, 41);
+            textBoxStep.TabIndex = 2;
+            // 
+            // btMix
+            // 
+            btMix.BackColor = SystemColors.Desktop;
+            btMix.Font = new Font("Tw Cen MT Condensed Extra Bold", 15.75F, FontStyle.Italic, GraphicsUnit.Point);
+            btMix.ForeColor = SystemColors.ControlLightLight;
+            btMix.Location = new Point(792, 319);
+            btMix.Name = "btMix";
+            btMix.Size = new Size(112, 36);
+            btMix.TabIndex = 3;
+            btMix.Text = "Xáo trộn";
+            btMix.UseVisualStyleBackColor = false;
+            // 
+            // btSolve
+            // 
+            btSolve.BackColor = SystemColors.Desktop;
+            btSolve.Font = new Font("Tw Cen MT Condensed Extra Bold", 15.75F, FontStyle.Italic, GraphicsUnit.Point);
+            btSolve.ForeColor = SystemColors.ButtonFace;
+            btSolve.Location = new Point(792, 419);
+            btSolve.Name = "btSolve";
+            btSolve.Size = new Size(112, 36);
+            btSolve.TabIndex = 4;
+            btSolve.Text = "Giải";
+            btSolve.UseVisualStyleBackColor = false;
+            // 
+            // btExit
+            // 
+            btExit.Font = new Font("Tw Cen MT", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btExit.Location = new Point(1172, 438);
+            btExit.Name = "btExit";
+            btExit.Size = new Size(114, 42);
+            btExit.TabIndex = 5;
+            btExit.Text = "Thoát";
+            btExit.UseVisualStyleBackColor = true;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("SimSun-ExtB", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(675, 170);
+            label1.Name = "label1";
+            label1.Size = new Size(125, 24);
+            label1.TabIndex = 6;
+            label1.Text = "Thời gian";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("SimSun-ExtB", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(894, 170);
+            label2.Name = "label2";
+            label2.Size = new Size(108, 24);
+            label2.TabIndex = 7;
+            label2.Text = "Số bước ";
+            // 
+            // label3
+            // 
+            label3.Font = new Font("Algerian", 48F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label3.ForeColor = Color.SeaGreen;
+            label3.Location = new Point(259, 21);
+            label3.Name = "label3";
+            label3.Size = new Size(875, 95);
+            label3.TabIndex = 8;
+            label3.Text = "Trò chơi 8 PUZZLE";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(1298, 482);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btExit);
+            Controls.Add(btSolve);
+            Controls.Add(btMix);
+            Controls.Add(textBoxStep);
+            Controls.Add(textBoxTime);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "Form1";
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button bt1;
+        private Button bt2;
+        private Button bt3;
+        private Button bt4;
+        private Button bt5;
+        private Button bt6;
+        private Button bt7;
+        private Button bt8;
+        private TextBox textBoxTime;
+        private TextBox textBoxStep;
+        private Button btMix;
+        private Button btSolve;
+        private Button btExit;
+        private ContextMenuStrip contextMenuStrip1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ 8_Puzzle/8_Puzzle/Form1.cs
@@ -0,0 +1,447 @@
+﻿namespace _8_Puzzle
+{
+    public partial class Form1 : Form
+    {
+        private int stepCount = 0;
+        private TableLayoutPanelCellPosition emptyTilePosition;
+        private System.Windows.Forms.Timer timer;
+        private int elapsedTime = 0;
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            bt1.Click += new EventHandler(Button_Click);
+            bt2.Click += new EventHandler(Button_Click);
+            bt3.Click += new EventHandler(Button_Click);
+            bt4.Click += new EventHandler(Button_Click);
+            bt5.Click += new EventHandler(Button_Click);
+            bt6.Click += new EventHandler(Button_Click);
+            bt7.Click += new EventHandler(Button_Click);
+            bt8.Click += new EventHandler(Button_Click);
+
+            btMix.Click += new EventHandler(btMix_Click);
+            btSolve.Click += new EventHandler(btSolve_ClickAsync);
+            btExit.Click += (sender, e) => { this.Close(); };
+
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+
+            emptyTilePosition = new TableLayoutPanelCellPosition(2, 2);
+            StartNewGame();
+        }
+
+        private void StartNewGame()
+        {
+            stepCount = 0;
+            elapsedTime = 0;
+            textBoxStep.Text = stepCount.ToString();
+            textBoxTime.Text = elapsedTime.ToString();
+            timer.Start();
+            ShuffleTiles();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            elapsedTime++;
+            textBoxTime.Text = elapsedTime.ToString();
+        }
+
+        private void ShuffleTiles()
+        {
+            List<int> numbers = Enumerable.Range(0, 9).OrderBy(x => random.Next()).ToList();
+            int index = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (numbers[index] == 0)
+                    {
+                        emptyTilePosition = new TableLayoutPanelCellPosition(j, i);
+                    }
+                    else
+                    {
+                        Button button = GetButtonByValue(numbers[index]);
+                        tableLayoutPanel1.SetCellPosition(button, new TableLayoutPanelCellPosition(j, i));
+                    }
+                    index++;
+                }
+            }
+        }
+
+        private Button GetButtonByValue(int value)
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                if (control is Button button && button.Text == value.ToString())
+                {
+                    return button;
+                }
+            }
+            return null;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            TableLayoutPanelCellPosition buttonPosition = tableLayoutPanel1.GetPositionFromControl(button);
+
+            if (IsValidMove(buttonPosition))
+            {
+                tableLayoutPanel1.SetCellPosition(button, emptyTilePosition);
+                emptyTilePosition = buttonPosition;
+                stepCount++;
+                textBoxStep.Text = stepCount.ToString();
+
+                if (IsSolved())
+                {
+                    timer.Stop();
+                    MessageBox.Show("You have solved the puzzle!");
+                }
+            }
+        }
+
+        private bool IsValidMove(TableLayoutPanelCellPosition position)
+        {
+            int rowDiff = Math.Abs(position.Row - emptyTilePosition.Row);
+            int colDiff = Math.Abs(position.Column - emptyTilePosition.Column);
+            return (rowDiff == 1 && colDiff == 0) || (rowDiff == 0 && colDiff == 1);
+        }
+
+        private bool IsSolved()
+        {
+            int[,] solution = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } };
+            for (int i = 0; i < tableLayoutPanel1.RowCount; i++)
+            {
+                for (int j = 0; j < tableLayoutPanel1.ColumnCount; j++)
+                {
+                    Control control = tableLayoutPanel1.GetControlFromPosition(j, i);
+                    if (control is Button button && int.TryParse(button.Text, out int number))
+                    {
+                        if (number != solution[i, j])
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        private List<List<int>> GetCurrentState()
+        {
+            List<List<int>> state = new List<List<int>>();
+            for (int row = 0; row < 3; row++)
+            {
+                List<int> currentRow = new List<int>();
+                for (int col = 0; col < 3; col++)
+                {
+                    Control control = tableLayoutPanel1.GetControlFromPosition(col, row);
+                    if (control is Button button && int.TryParse(button.Text, out int number))
+                    {
+                        currentRow.Add(number);
+                    }
+                    else
+                    {
+                        currentRow.Add(0);
+                    }
+                }
+                state.Add(currentRow);
+            }
+            return state;
+        }
+
+        private void SetState(List<List<int>> state)
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    int number = state[row][col];
+                    Control control = tableLayoutPanel1.GetControlFromPosition(col, row);
+
+                    if (number == 0)
+                    {
+                        emptyTilePosition = new TableLayoutPanelCellPosition(col, row);
+                    }
+                    else
+                    {
+                        Button button = GetButtonByValue(number);
+                        tableLayoutPanel1.SetCellPosition(button, new TableLayoutPanelCellPosition(col, row));
+                    }
+                }
+            }
+        }
+
+        private void btMix_Click(object sender, EventArgs e)
+        {
+            StartNewGame();
+        }
+
+        private async void btSolve_ClickAsync(object sender, EventArgs e)
+        {
+            timer.Stop();
+            List<List<int>> initialState = GetCurrentState();
+            List<List<int>> goalState = new List<List<int>>
+            {
+                new List<int> { 1, 2, 3 },
+                new List<int> { 4, 5, 6 },
+                new List<int> { 7, 8, 0 }
+            };
+
+            List<State> solutionPath = await Task.Run(() => AStarSearch(initialState, goalState));
+            if (solutionPath != null)
+            {
+                foreach (State state in solutionPath)
+                {
+                    SetState(state.Board);
+                    await Task.Delay(500); // Delay to visualize the steps
+                }
+                MessageBox.Show("Puzzle Solved!");
+                StartNewGame(); // Bắt đầu trò chơi mới sau khi giải quyết thành công
+            }
+            else
+            {
+                MessageBox.Show("No solution found!");
+            }
+        }
+
+        private List<State> AStarSearch(List<List<int>> initialState, List<List<int>> goalState)
+        {
+            PriorityQueue<State> openSet = new PriorityQueue<State>();
+            HashSet<State> closedSet = new HashSet<State>();
+
+            State startState = new State(initialState, null, 0, Heuristic(initialState, goalState));
+            openSet.Enqueue(startState);
+
+            while (openSet.Count > 0)
+            {
+                State currentState = openSet.Dequeue();
+                if (currentState.Equals(goalState))
+                {
+                    return ReconstructPath(currentState);
+                }
+
+                closedSet.Add(currentState);
+
+                foreach (State neighbor in currentState.GetNeighbors())
+                {
+                    if (closedSet.Contains(neighbor))
+                    {
+                        continue;
+                    }
+
+                    int tentativeGScore = currentState.G + 1;
+
+                    if (!openSet.Contains(neighbor) || tentativeGScore < neighbor.G)
+                    {
+                        neighbor.Previous = currentState;
+                        neighbor.G = tentativeGScore;
+                        neighbor.F = tentativeGScore + Heuristic(neighbor.Board, goalState);
+
+                        if (!openSet.Contains(neighbor))
+                        {
+                            openSet.Enqueue(neighbor);
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        private int Heuristic(List<List<int>> state, List<List<int>> goal)
+        {
+            int h = 0;
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    int value = state[row][col];
+                    if (value != 0)
+                    {
+                        for (int goalRow = 0; goalRow < 3; goalRow++)
+                        {
+                            for (int goalCol = 0; goalCol < 3; goalCol++)
+                            {
+                                if (goal[goalRow][goalCol] == value)
+                                {
+                                    h += Math.Abs(row - goalRow) + Math.Abs(col - goalCol);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return h;
+        }
+
+        private List<State> ReconstructPath(State state)
+        {
+            List<State> path = new List<State>();
+            while (state != null)
+            {
+                path.Insert(0, state);
+                state = state.Previous;
+            }
+            return path;
+        }
+
+        public class State : IComparable<State>
+        {
+            public List<List<int>> Board { get; }
+            public State Previous { get; set; }
+            public int G { get; set; }
+            public int F { get; set; }
+
+            public State(List<List<int>> board, State previous, int g, int f)
+            {
+                Board = board;
+                Previous = previous;
+                G = g;
+                F = f;
+            }
+
+            public List<State> GetNeighbors()
+            {
+                List<State> neighbors = new List<State>();
+                (int row, int col) = FindEmptyTile();
+
+                foreach ((int dRow, int dCol) in new (int, int)[] { (-1, 0), (1, 0), (0, -1), (0, 1) })
+                {
+                    int newRow = row + dRow;
+                    int newCol = col + dCol;
+
+                    if (newRow >= 0 && newRow < 3 && newCol >= 0 && newCol < 3)
+                    {
+                        List<List<int>> newBoard = Board.Select(innerList => innerList.ToList()).ToList();
+                        newBoard[row][col] = newBoard[newRow][newCol];
+                        newBoard[newRow][newCol] = 0;
+                        neighbors.Add(new State(newBoard, this, G + 1, 0));
+                    }
+                }
+                return neighbors;
+            }
+
+            private (int, int) FindEmptyTile()
+            {
+                for (int row = 0; row < 3; row++)
+                {
+                    for (int col = 0; col < 3; col++)
+                    {
+                        if (Board[row][col] == 0)
+                        {
+                            return (row, col);
+                        }
+                    }
+                }
+                return (-1, -1);
+            }
+
+            public bool Equals(List<List<int>> otherBoard)
+            {
+                for (int row = 0; row < 3; row++)
+                {
+                    for (int col = 0; col < 3; col++)
+                    {
+                        if (Board[row][col] != otherBoard[row][col])
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+
+            public int CompareTo(State other)
+            {
+                return F.CompareTo(other.F);
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is State other && Equals(other.Board);
+            }
+
+            public override int GetHashCode()
+            {
+                int hash = 17;
+                foreach (var row in Board)
+                {
+                    foreach (var value in row)
+                    {
+                        hash = hash * 31 + value;
+                    }
+                }
+                return hash;
+            }
+        }
+
+        public class PriorityQueue<T> where T : IComparable<T>
+        {
+            private List<T> data;
+
+            public PriorityQueue()
+            {
+                this.data = new List<T>();
+            }
+
+            public void Enqueue(T item)
+            {
+                data.Add(item);
+                int ci = data.Count - 1;
+                while (ci > 0)
+                {
+                    int pi = (ci - 1) / 2;
+                    if (data[ci].CompareTo(data[pi]) >= 0) break;
+                    T tmp = data[ci];
+                    data[ci] = data[pi];
+                    data[pi] = tmp;
+                    ci = pi;
+                }
+            }
+
+            public T Dequeue()
+            {
+                int li = data.Count - 1;
+                T frontItem = data[0];
+                data[0] = data[li];
+                data.RemoveAt(li);
+
+                li--;
+                int pi = 0;
+                while (true)
+                {
+                    int ci = pi * 2 + 1;
+                    if (ci > li) break;
+                    int rc = ci + 1;
+                    if (rc <= li && data[rc].CompareTo(data[ci]) < 0)
+                        ci = rc;
+                    if (data[pi].CompareTo(data[ci]) <= 0) break;
+                    T tmp = data[pi];
+                    data[pi] = data[ci];
+                    data[ci] = tmp;
+                    pi = ci;
+                }
+                return frontItem;
+            }
+
+            public int Count
+            {
+                get { return data.Count; }
+            }
+
+            public bool Contains(T item)
+            {
+                return data.Contains(item);
+            }
+        }
+    }
+}
